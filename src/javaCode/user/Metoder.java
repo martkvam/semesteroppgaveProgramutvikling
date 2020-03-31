f5ced373bc9908a73439f0b68e2201c4522c794b
Martin K <martin_kvam@hotmail.com>
2020-03-31 19:20:25 +0200

    Forhåpentligvis ferdig med choiceboxer og tableview

---
+++ src/javaCode/user/Metoder.java
@@ -0,0 +1,58 @@
+package javaCode.user;
+
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+
+public class Metoder {
+    //Metode for å lage liste som skal fylle biltype-choicebox
+    public static ArrayList<String> typeList(ObservableList<Car> inList) {
+        ArrayList<Car> aList = new ArrayList<>();
+        for (Car car : inList) {
+            char ID = car.getCarID().charAt(0);
+            boolean found = false;
+            for(int i = 0; i<aList.size(); i++){
+                char ID2 = aList.get(i).getCarID().charAt(0);
+                if (ID == ID2) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found){
+                aList.add(car);
+            }
+        }
+
+        ArrayList<String> outList = new ArrayList<>();
+        for(Car car : aList){
+            outList.add(car.getCarType());
+        };
+        return outList;
+
+    }
+
+    //Metode for å lage liste som skal fylle komponent-choicebox
+    public static ArrayList<String> componentList(ObservableList<Component> inList){
+        ArrayList<Component> aList = new ArrayList<>();
+        for (Component component : inList) {
+            char ID = component.getComponentID().charAt(0);
+            boolean found = false;
+            for(int i = 0; i<aList.size(); i++){
+                char ID2 = aList.get(i).getComponentID().charAt(0);
+                if (ID == ID2) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found){
+                aList.add(component);
+            }
+        }
+
+        ArrayList<String> outList = new ArrayList<>();
+        for(Component component : aList){
+            outList.add(component.getComponentType());
+        };
+        return outList;
+    }
+}

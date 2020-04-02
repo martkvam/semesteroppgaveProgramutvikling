438b2b40519d78f10552997ca72b2dd9e3e395a0
Trym <s341827@oslomet.no>
2020-04-02 11:31:40 +0200

    Prøver å fikse branchen

--- src/javaCode/Main.java
+++ src/javaCode/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("../resources/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../resources/Inlog.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root,900,800));
+        primaryStage.setScene(new Scene(root,500,600));
         primaryStage.show();
     }
 

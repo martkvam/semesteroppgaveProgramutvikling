47d662b4fc4f400dc9c4a67d77ab2ddc5b51bdf9
Adrian Nilsen <adrian.bn@hotmail.com>
2020-04-10 00:41:33 +0200

    Små endringer komponent ny

--- src/javaCode/superUser/ControllerAddEditComponents.java
+++ src/javaCode/superUser/ControllerAddEditComponents.java
@@ -4,15 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 
